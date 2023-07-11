20d8a935bd94241f10f2c7a9883407bd1cadb66a
apmapmapm <alexpvpmindustry@github.com>
2023-07-11 13:00:45 +0800

    ver2

---
+++ master_trades_ver2.py
@@ -0,0 +1,72 @@
+import datetime,time,sys
+from trader import get_current_price,price_action_signal, read_signal
+import json
+from disc_api import ping,STATUS_PING2,SIGNALROLE,CRYPTO_SIGNALS2
+from trader import market_trade 
+
+## input arguments
+param_choice = 0 
+if len(sys.argv)>1:
+    param_choice = int(sys.argv[1])
+trade_params = json.load(open("trade_params.json","r"))
+trade_param = trade_params["ver2"]["params"][param_choice]
+thres_diff = float(trade_param["thres_diff"])
+tickerpair = trade_param["tickerpair"]
+interval = trade_param["interval"]
+sl = float(trade_param["sl"])
+tp = float(trade_param["tp"])
+percentile = float(trade_param["percentile"])
+hl_pairs = None
+
+#enter trade:
+symbol = tickerpair
+qtyUSD = float(sys.argv[2])
+dfname = sys.argv[3]
+test=(sys.argv[4]=="TEST")
+
+cur_price = get_current_price(symbol,sell=False)
+qty = qtyUSD/cur_price
+a1,a2,a3 = market_trade(symbol,qty,buy=True,test=test)
+
+if a1=="FILLED":# we have entered the trade
+    ping(CRYPTO_SIGNALS2,f"We have entered the trade pc{param_choice}, {cur_price:.4f}")
+else:
+    ping(CRYPTO_SIGNALS2,f"ENTER ERROR pc{param_choice} {SIGNALROLE}")
+    assert False
+# continous loop until exit or sell signal is recieved
+status="HOLD"
+loopcounts=0
+
+ent_time=str(datetime.datetime.now()).replace(" ","_")
+enter_data = {"price":cur_price,"sl":sl,"tp":tp,"dfname":dfname,"ent_time":ent_time,
+              "hl_pairs":hl_pairs,"strat":"strat_tpsl1"}
+strat_data = {"cur_sl":sl,"cur_tp":tp,"slip":-0.002,"ent_sl":sl,"ent_tp":tp,"strat":"strat_tpsl1"}
+
+stdmean_status="HOLD"
+while status=="HOLD":
+    # get current price
+    cur_price = get_current_price(symbol)
+    # get price_action_signal
+    pas_status,strat_data,strr = price_action_signal(enter_data,strat_data,cur_price)
+    if strr[:2]=="Up": # shifting of SLTP
+        ping(CRYPTO_SIGNALS2,pas_status+f" pc{param_choice} "+strr)
+    if loopcounts%10==0:# read exit status
+        #TODO read exit status
+        stdmean_status=read_signal(symbol,interval)
+        if stdmean_status != "EXIT":
+            stdmean_status = "HOLD"
+    status ="HOLD" if ((stdmean_status=="HOLD") and (pas_status=="HOLD")) else "SELL"
+    loopcounts+=1
+    if status=="HOLD":
+        time.sleep(3)
+
+# sell position
+a1,a2,a3 = market_trade(symbol,qty,buy=False,test=test)
+if a1=="FILLED":# we have exited the trade
+    change = (cur_price-enter_data['price'])/enter_data['price']
+    strr = f"We have exited the trade pc{param_choice}, {cur_price:.4f},"
+    strr+= f" entered at {enter_data['price']:.4f},\nsize=${qtyUSD} ({change*100:.2f}%, {qtyUSD*change:.2f}$)"
+    ping(CRYPTO_SIGNALS2,strr)
+else:
+    ping(CRYPTO_SIGNALS2,f"EXIT ERROR pc{param_choice} {SIGNALROLE}")
+    assert False
---
+++ run_trades_ver2.py
@@ -0,0 +1,112 @@
+
+import subprocess
+import sys 
+from glob import glob
+import time
+from itertools import zip_longest
+import json,datetime,schedule
+
+from trader import write_signal 
+from funcs import get_data,get_entrys_exits,get_entry_signals,read_signal
+from disc_api import ping,STATUS_PING2,SIGNALROLE,CRYPTO_SIGNALS2,ERROR_PING2
+
+## input arguments
+param_choice = 0 
+if len(sys.argv)>1:
+    param_choice = int(sys.argv[1])
+
+trade_params = json.load(open("trade_params.json","r"))
+trade_param = trade_params["ver2"]["params"][param_choice]
+thres_diff = float(trade_param["thres_diff"])
+tickerpair = trade_param["tickerpair"]
+interval = trade_param["interval"]
+sl = float(trade_param["sl"])
+tp = float(trade_param["tp"])
+percentile = float(trade_param["percentile"])
+hl_pairs = None
+
+entered=False
+new_entry=False
+
+def get_signal():
+    global new_entry, entered,thres_diff,percentile,interval,tickerpair 
+    dfmpl = get_data(tickerpair,interval,limit=100,type="live")
+    entrys,exits,_,_,_,_,_,_,thres_diff = get_entrys_exits(dfmpl,percentile,thres_diff)
+    #get all entry signal
+    entry_signals = get_entry_signals(entrys,dfmpl,onlybuy=True) 
+    new_entry=False
+    entry_df= None;buy=None
+    for entry,exit in zip_longest(entrys,exits,fillvalue=None):
+        if exit is None: # missing exit signal, so its a new enter signal
+            buy_list = [ buy for entry_, buy in entry_signals if entry_==entry]
+            if len(buy_list)==1: 
+                new_entry=True
+                entry_df=dfmpl.iloc[entry]
+                buy=buy_list[0]
+    if new_entry:
+        assert buy==1
+    if new_entry and entered:
+        #print("hold")
+        write_signal(tickerpair,interval,signal="HOLD",closeprice=dfmpl.iloc[-1].Close,dfname=dfmpl.iloc[-1].name) 
+    elif not new_entry and entered:
+        #print("exit trade now")
+        entered=False
+        strr = f"EXIT pc{param_choice} `{tickerpair}` `{interval}` `{dfmpl.iloc[-1].Close}`"
+        strr+= f"`{dfmpl.iloc[-1].name}` (`{str(datetime.datetime.now())}`) {SIGNALROLE}"
+        write_signal(tickerpair,interval,signal="EXIT",closeprice=dfmpl.iloc[-1].Close,dfname=dfmpl.iloc[-1].name) 
+        ping(CRYPTO_SIGNALS2,strr)#requests.post(config["crypto-signals2"],data={"content":strr})
+    elif new_entry and not entered:
+        xx = entry_df.Close
+        strr = f"pc{param_choice} `{tickerpair}` `{interval}` "+ ("BUY  " if buy==1 else "SELL ")+f"`{xx}`" 
+        strr += f" tp `{xx*(1+tp):.4f}` sl `{xx*(1+sl):.4f}`\n"
+        strr += f"`{entry_df.name}` (`{str(datetime.datetime.now())}`) {SIGNALROLE}"
+        write_signal(tickerpair,interval,signal="ENTER",closeprice=dfmpl.iloc[-1].Close,dfname=dfmpl.iloc[-1].name)
+        # execute trading algo
+        enterdftime = str(dfmpl.iloc[-1].name).replace(" ","_")
+        cmd = ["python","master_trades_ver2.py",param_choice,"15",f"{enterdftime}","TEST"]
+        subprocess.Popen( cmd , shell=True)
+        ping(CRYPTO_SIGNALS2,strr)#requests.post(config["crypto-signals2"],data={"content":strr})
+        entered=True
+    else:
+        strr = f"fetched pc{param_choice} `{tickerpair}` `{interval}` at `{dfmpl.iloc[-1].name}`"
+        strr+= f"(`{str(datetime.datetime.now())}`)"
+        ping(STATUS_PING2,strr)#requests.post(config["status-ping2"],data={"content":strr})
+    ddtn=datetime.datetime.now()
+    print(f"last ran:{ddtn}")
+def get_signal_with_warnings():
+    try:
+        get_signal()
+    except Exception as e:
+        ping(ERROR_PING2,f"error pc{param_choice} "+str(e))
+        raise
+print("starting")
+intvl = int(interval.split("m")[0]); ddtn=datetime.datetime.now()
+delay = (intvl-1-ddtn.minute%intvl)*60+(60-ddtn.second+12) # 12 seconds after 
+time.sleep(delay)
+schedule.every(intvl).minutes.at(":02").do(get_signal_with_warnings) # run this at 22:30:04 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
