3a6708ed2f738fde103839ad6c054664a70aa5ca
alex <alexpvpmindustry@github.com>
2023-07-11 11:06:10 +0800

    eh

---
+++ activate_run_script.sh
@@ -0,0 +1,3 @@
+conda activate cryt310
+echo $1
+#python run_trades0_0_0.py $1
---
+++ run_commands.sh
@@ -0,0 +1,6 @@
+
+# loop through all runs
+for i in {4..5}; do screen -dmS "run$i" sh -c "/root/miniconda3/condabin/conda activate cryt310; echo $i; conda env list; exec bash;"; done
+
+# kill all screens
+screen -ls | grep '(Detached)' | awk '{print $1}' | xargs -I % -t screen -X -S % quit
