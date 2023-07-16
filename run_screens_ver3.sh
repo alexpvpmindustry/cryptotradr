2e7681a7281e979fd27e1df973354e1cd036fed0
alex <alexpvpmindustry@github.com>
2023-07-16 09:12:19 +0800

    erg

--- run_screens_ver3.sh
+++ run_screens_ver3.sh
@@ -1,4 +1,4 @@
 
-screen -dmS "run5m" sh -c "/root/miniconda3/condabin/conda activate cryt310_try; echo running5m; python run_trades_ver3.py $1 $2 5m; exec bash;";
-screen -dmS "run15m" sh -c "/root/miniconda3/condabin/conda activate cryt310_try; echo running15m; python run_trades_ver3.py $1 $2 15m; exec bash;";
-screen -dmS "run30m" sh -c "/root/miniconda3/condabin/conda activate cryt310_try; echo running30m; python run_trades_ver3.py $1 $2 30m; exec bash;";
+screen -dmS "run5m" sh -c "/root/miniconda3/condabin/conda activate cryt310; echo running5m; python run_trades_ver3.py $1 $2 5m; exec bash;";
+#screen -dmS "run15m" sh -c "/root/miniconda3/condabin/conda activate cryt310_try; echo running15m; python run_trades_ver3.py $1 $2 15m; exec bash;";
+#screen -dmS "run30m" sh -c "/root/miniconda3/condabin/conda activate cryt310_try; echo running30m; python run_trades_ver3.py $1 $2 30m; exec bash;";
