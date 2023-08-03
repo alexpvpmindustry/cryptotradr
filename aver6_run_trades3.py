e89d165722b790e659f140d81a84d0ab7d43f076
apmapmapm <alexpvpmindustry@github.com>
2023-08-03 18:45:01 +0800

    antoher moenertg

---
+++ aver6_run_trades3.py
@@ -0,0 +1,76 @@
+import asyncio
+from binance import AsyncClient, BinanceSocketManager
+from binance.enums import *
+import time
+import datetime
+from collections import Counter
+import pickle
+import subprocess
+from disc_api import ALEXPING, get_random_emoji, ping,STATUS_PING2,SIGNALROLE,CRYPTO_SIGNALS2,ERROR_PING2,CRYPTO_LOGS2
+import traceback
+with open("9_0_subset_symbols_24hrchange.pkl","rb") as f:
+    subset_symbols = pickle.load(f)
+maxsymbols=-1
+master_list=[[None] for _ in subset_symbols[:maxsymbols]]
+master_list_status=["1111" for _ in subset_symbols[:maxsymbols]]
+MOMENTUM_count=0
+async def main(symbol='BNBBTC',idd=0):
+    global master_list ,MOMENTUM_count,master_list_status
+    await asyncio.sleep(idd*0.25)
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client) 
+    ts = bm.kline_socket(symbol, interval=KLINE_INTERVAL_1MINUTE) 
+    print(f"sub{idd}",end=" ")
+    prev="0000";prev_df=[None]
+    async with ts as tscm:
+        while True:
+            try:
+                res = await tscm.recv() 
+                if res["e"]=="error":
+                    print(str(datetime.datetime.now())[11:-4],res)
+                    raise Exception(res) 
+                if prev != str(res["k"]["T"])[6:-3]: # a new change, so append the prev_df into the master_list
+                    prev = str(res["k"]["T"])[6:-3]
+                    
+                    ress=res["k"]
+                    df = [ress["t"],float(ress["o"]),float(ress["c"]),float(ress["v"])] 
+                    master_list[idd].append(prev_df.copy())
+                    master_list_status[idd]=prev
+                    prev_df = df.copy()
+                    if len(master_list[idd])>2:
+                        master_list[idd].pop(0)
+                        if (master_list[idd][0][0] is not None) and (master_list[idd][1][0] is not None):
+                            # work on master_list since it has the latest dataset
+                            dfloc0 = master_list[idd][0];dfloc1=master_list[idd][1]
+                            v0 = dfloc0[1]*dfloc0[3];v1 = dfloc1[1]*dfloc1[3];
+                            g0 = (dfloc0[2]-dfloc0[1])/dfloc0[1]
+                            g1 = (dfloc1[2]-dfloc1[1])/dfloc1[1]
+                            if v0>1e6 and v1>1e6 and g0<-0.006 and g1<-0.006:
+                                #BUY signal!
+                                cmd = ["python","aver6_master_trades.py",symbol,"15",str(datetime.datetime.now())[:-4],
+                                       "TEST",f"{dfloc1[2]:.6g}","-0.006","-0.006","MOMENTU3",f"{MOMENTUM_count}"]
+                                cmd = " ".join(cmd)
+                                subprocess.Popen( cmd , shell=True)
+                                MOMENTUM_count+=1
+                            if idd==0:
+                                strr=f"MOMENTUM {str(datetime.datetime.now())[:-4]},"
+                                strr+=f"sync{Counter(master_list_status)}, opos={MOMENTUM_count}"
+                                ping(STATUS_PING2,strr)
+            except Exception as e:
+                strr=traceback.format_exc()
+                print("ERROR",symbol,e,str(e))
+                ping(ERROR_PING2,f"MOMENT3 error {symbol} {ALEXPING} "+str(e)+"  "+strr+f" dfloc0{dfloc0} dfloc1{dfloc1}")
+                break
+    await client.close_connection()
+    print(f"ended {symbol}") 
+
+ddtn=datetime.datetime.now()
+rand_emoji = f"{get_random_emoji()}{get_random_emoji()}{get_random_emoji()}"
+
+ping(ERROR_PING2,f"new run {ddtn},{rand_emoji}")
+loop = asyncio.get_event_loop() 
+print("starting event loop")
+for idd,s in enumerate(subset_symbols[:maxsymbols]):
+    asyncio.run_coroutine_threadsafe(main(s+"USDT",idd), loop)
+loop.run_forever()
+print("ending loop")
