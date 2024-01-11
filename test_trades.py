df3bfadd259f38fdeca428f555ced591e70c5cee
apmapmapm <alexpvpmindustry@github.com>
2024-01-11 10:47:56 +0800

    some changes

--- test_trades.py
+++ test_trades.py
@@ -1,14 +1,14 @@
 from aver6_trader import market_trade
 
 print("buy")
-a1,a2,a3 = market_trade("LTCUSDT",0.25,True,False)
+a1,a2,a3 = market_trade("LTCUSDT",0.25,buy=True,test=False)
 
 print(a1)
 print(a2)
 print(a3)
 
 print("selling")
-a1,a2,a3 = market_trade("LTCUSDT",0.25,False,False)
+a1,a2,a3 = market_trade("LTCUSDT",0.25,buy=False,test=False)
 
 print(a1)
 print(a2)
